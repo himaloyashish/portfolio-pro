c5e12e9ee1fef537f25f5f9a358d458b6dd52350
himaloyash <himaloyashis98@gmail.com>
2024-01-06 23:23:45 +0600

    navbar done

--- src/app/navbar/page.jsx
+++ src/app/navbar/page.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+"use client"
+import {  XMarkIcon, Bars3Icon } from '@heroicons/react/24/solid'
+import { useState } from 'react';
 
 const Navbar = () => {
+
+    const [Open, setOpen] = useState(false)
+
     return (
         <div className=' py-4 px-2 bg-[#304D30] text-white'>
-            <ul className=' lg:flex  justify-between items-center '>
+            <span className='lg:hidden' onClick={() => setOpen(!Open)}>
+                {
+                    Open ? <XMarkIcon className="h-6 w-6 text-blue-500 cursor-pointer" /> :
+                        <Bars3Icon className="h-6 w-6 text-blue-500 cursor-pointer" />
+
+                }
+            </span>
+
+            <ul className={` lg:flex  justify-between items-center absolute z-10  md:static ${Open? "left-6" : "-left-28 duration-500"} `}>
                 <ul>
-                    <li><a href="#">Himaloy</a></li>
+                    <li className='font-bold text-xl '><a href="#">Himaloy</a></li>
                 </ul>
                 <ul className='lg:flex gap-3'>
                     <li className='font-bold text-xl '><a href="#home">Home</a></li>
